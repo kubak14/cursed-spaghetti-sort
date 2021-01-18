68629194e28dcba3348b831b1bfd9e26068b2a9b
kubak14 <66217082+kubak14@users.noreply.github.com>
2021-01-18 16:22:17 +0100

    New version of sort
    + Added autotesting
    + Added new features (descending sorting, sorting by key)
    + Fixed bugs
    + Updated comments

--- spaghetti_sort.py
+++ spaghetti_sort.py
@@ -1,5 +1,11 @@
-def spaghetti(iterable) -> 'sorted iterable':
-    "Returns sorted iterable"
+##author: github.com/kubak14
+##best time complexity: O(n)
+##average time complexity: O(x - y) where x is the biggest number and y is the smallest number in list
+##worst time complexity: O(x - y)
+##for more information, please visit README file: github.com/kubak14/REDME
+
+def spaghetti(iterable, key = None, descending = False) -> 'sorted list':
+    "Returns sorted list"
 
     rod_heights = dict()
 ##  this dictionary holds the heights of successive rods as keys and
@@ -13,17 +19,24 @@ def spaghetti(iterable) -> 'sorted iterable':
     for elem in iterable:
 ##      in the first iteration we have to find the highest rod
 ##      and convert list into dictionary
-        if elem in rod_heights: #if element is already in dictionary
-            rod_heights[elem].append(elem) #we can add current element to it
+        if key != None:
+            v = key(elem) #swapping variable elem with attribute of the elem provided by user
+        else:
+            v = elem
+
+            
+        if v in rod_heights: #if element is already in dictionary
+            rod_heights[v].append(elem) #we can add current element to it
 
         else:
-            rod_heights[elem] = [elem] #else we have to create it first
+            rod_heights[v] = [elem] #else we have to create it first
 
-        if elem > max_value: #checking if the current element is bigger than current biggest value
-            max_value = elem
 
-        if elem < min_value: #checking if the current element is smaller than current smallest value
-            min_value = elem
+        if v > max_value: #checking if the current element is bigger than current biggest value
+            max_value = v
+
+        if v < min_value: #checking if the current element is smaller than current smallest value
+            min_value = v
 
     length = len(iterable) #capturing current length of iterable
     iterable.clear() #clearing iterable, so we won't have to create more data structures
@@ -35,7 +48,8 @@ def spaghetti(iterable) -> 'sorted iterable':
         elem_list = rod_heights.get(h, None) #this variable holds elements under index h(if there is no index h it returns None instead of error)
 
         if elem_list != None: #if index exist
-            iterable = elem_list + iterable #here, we are adding elements to the start of iterable
+            iterable = elem_list + iterable if not descending else iterable + elem_list
+##          here, we are adding elements to the start or end of iterable depending on arguments sended to function
 
             if len(iterable) == length:
 ##              if current length of iterable is equal to length of iterable before clear,
@@ -43,4 +57,7 @@ def spaghetti(iterable) -> 'sorted iterable':
                 break
 
     return iterable
-    
+
+
+if __name__ == '__main__':
+    print(spaghetti([5, 9, 2, 0, 10, 3, 1, 8, 500, 7, 4, 6, 10]))
